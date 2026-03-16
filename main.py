50fc267776313d37d070e7a5e3ab25cf65b640b0
AloysioBourget <bourget.gustavo@gmail.com>
2026-03-16 20:33:20 -0300

    Alteração

---
+++ main.py
@@ -0,0 +1,6 @@
+# System of Financial Analysis - Version 0.1
+def main():
+    print("System of Financial Analysis - Version 0.1")
+    print("==========================================")
+    
+main()
